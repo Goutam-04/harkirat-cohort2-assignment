a1586e2b4e4ab4b6fb409aa4a42d5006fdc906c8
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-06-18 21:04:16 +0530

    week 3 task 3

--- week-3/03-mongo/db/db/index.js
+++ week-3/03-mongo/db/db/index.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://robinhooddune:J6koWbkNH9iDf5H3@cluster0.7oa32cu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test');
+mongoose.connect('mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/course_selling_app');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-   username: String,
+    // Schema definition here
+    username: String,
     password: String
 });
 
 const UserSchema = new mongoose.Schema({
+    // Schema definition here
     username: String,
     password: String,
     purchasedCourses: [{
@@ -19,6 +21,7 @@ const UserSchema = new mongoose.Schema({
 });
 
 const CourseSchema = new mongoose.Schema({
+    // Schema definition here
     title: String,
     description: String,
     imageLink: String,
